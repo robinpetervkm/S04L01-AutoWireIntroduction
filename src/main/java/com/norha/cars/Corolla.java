7e5586ec98b26bd5a6f5fc9b3d85c62b13bcbfe7
Robinclear <robin.peter.vkm@gmail.com>
2020-03-30 18:27:25 -0300

    Exception in thread "main" java.lang.NullPointerException

--- src/main/java/com/norha/cars/Corolla.java
+++ src/main/java/com/norha/cars/Corolla.java
@@ -5,9 +5,13 @@ import org.springframework.stereotype.Component;
 import com.norha.interfaces.Car;
 @Component("MyCustomName")
 public class Corolla implements Car  {
+	
+	Engine engine;
 
 	public String specs() {
-		return "Sedan from Toyotta";
+		
+		String speString = "Sedan From Toyota With Engine Type as "+engine.getType();
+		return speString;
 	}
 
 }
---
+++ src/main/java/com/norha/cars/Engine.java
@@ -0,0 +1,20 @@
+package com.norha.cars;
+
+public class Engine {
+	
+	String type;
+
+	public Engine() {
+		type="V8";
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+
+}
